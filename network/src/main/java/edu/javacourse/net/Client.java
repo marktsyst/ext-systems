364ac7eb25b9812777542480af813a1447de6a82
marktsyst <mark@tsyst.ru>
2023-09-21 16:23:20 +1000

    multithread

--- network/src/main/java/edu/javacourse/net/Client.java
+++ network/src/main/java/edu/javacourse/net/Client.java
@@ -2,18 +2,26 @@ package edu.javacourse.net;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.time.LocalDateTime;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        for (int i = 0; i < 7; i++) {
-            sendRequest();
+        for(int i=0; i<10; i++) {
+            SimpleClient sc = new SimpleClient();
+            sc.start();
         }
     }
+}
 
-    private static void sendRequest() throws IOException {
+class SimpleClient extends Thread {
+    @Override
+    public void run() {
         try (Socket socket = new Socket("127.0.0.1", 25225);
              BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+
+            System.out.println("Started:" + LocalDateTime.now());
 
             String userName = "Антон";
             bw.write(userName);
@@ -22,6 +30,12 @@ public class Client {
 
             String answer = br.readLine();
             System.out.println("Client got string:" + answer);
+
+            System.out.println("Finished:" + LocalDateTime.now());
+        } catch (UnknownHostException e) {
+            e.printStackTrace(System.out);
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
         }
     }
 }
--- network/src/main/java/edu/javacourse/net/Server.java
+++ network/src/main/java/edu/javacourse/net/Server.java
@@ -7,25 +7,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Server {
-    public static Map<String, Integer> userCount = new HashMap<>();
-    public static Map<Integer, String> answerDict = Map.of(
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket socket = new ServerSocket(25225, 200)) {
+            System.out.println("Server is started");
+            while (true) {
+                Socket client = socket.accept();
+                new SimpleServer(client).start();
+            }
+        }
+    }
+}
+
+class SimpleServer extends Thread {
+    private static Map<String, Integer> userCount = new HashMap<>();
+    private static Map<Integer, String> answerDict = Map.of(
             0, "Привет",
             1, "Виделись уже",
             2, "Снова ты",
             3, "Да сколько можно",
             4, "Заманал ты");
 
-    public static void main(String[] args) throws IOException {
-        try (ServerSocket socket = new ServerSocket(25225)) {
-            System.out.println("Server is started");
-            while (true) {
-                Socket client = socket.accept();
-                handleRequest(client);
-            }
-        }
+    private Socket client;
+
+    public SimpleServer(Socket client) {
+        this.client = client;
     }
 
-    private static void handleRequest(Socket client) throws IOException {
+    @Override
+    public void run() {
+        handleRequest(client);
+    }
+    private void handleRequest(Socket client) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
              BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()))) {
 
@@ -34,15 +46,18 @@ public class Server {
             String answer = getAnswer(userName);
 
             System.out.println("Server got string:" + userName);
+            Thread.sleep(2000);
             StringBuilder sb = new StringBuilder(answer).append(", ").append(userName);
             bw.write(sb.toString());
             bw.newLine();
             bw.flush();
+            client.close();
+        } catch (InterruptedException  | IOException ex) {
+            ex.printStackTrace(System.out);
         }
-        client.close();
     }
 
-    private static String getAnswer(String userName) {
+    private String getAnswer(String userName) {
         var count = userCount.get(userName);
         if (count == null) {
             count = 0;
