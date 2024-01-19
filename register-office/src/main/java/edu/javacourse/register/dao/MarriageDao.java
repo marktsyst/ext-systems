2a17f5466c466631b3ca70ad394c8e39419d2de6
marktsyst <mark@tsyst.ru>
2024-01-19 20:43:05 +1000

    spring annotation 122

--- register-office/src/main/java/edu/javacourse/register/dao/MarriageDao.java
+++ register-office/src/main/java/edu/javacourse/register/dao/MarriageDao.java
@@ -6,11 +6,17 @@ import edu.javacourse.register.view.MarriageRequest;
 import jakarta.persistence.EntityManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MarriageDao {
     private static final Logger LOGGER = LoggerFactory.getLogger(MarriageDao.class);
 
     private EntityManager entityManager;
+
+    @Value("${test.value}")
     private String test;
 
     public void setTest(String test) {
