54aa5f8c8e123e023e251c64d7db5cd3f3b10975
marktsyst <mark@tsyst.ru>
2024-01-19 12:21:21 +1000

    domain 117

--- register-office/src/main/java/edu/javacourse/register/domain/Passport.java
+++ register-office/src/main/java/edu/javacourse/register/domain/Passport.java
@@ -1,12 +1,34 @@
 package edu.javacourse.register.domain;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.time.LocalDate;
 
+@Table(name = "ro_passport")
+@Entity
 public class Passport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "passport_id")
     private Long passportId;
+    @ManyToOne(cascade = {CascadeType.REFRESH}, fetch = FetchType.LAZY)
+    @JoinColumn(name = "person_id")
+    private Person person;
+    @Column(name = "seria")
     private String seria;
+    @Column(name = "number")
     private String number;
+    @Column(name = "date_issue")
     private LocalDate issueDate;
+    @Column(name = "issue_department")
     private String issueDepartment;
 
     public Long getPassportId() {
@@ -15,6 +37,14 @@ public class Passport {
 
     public void setPassportId(Long passportId) {
         this.passportId = passportId;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
     }
 
     public String getSeria() {
--- register-office/src/main/java/edu/javacourse/register/domain/Person.java
+++ register-office/src/main/java/edu/javacourse/register/domain/Person.java
@@ -1,14 +1,33 @@
 package edu.javacourse.register.domain;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.time.LocalDate;
 import java.util.List;
 
+@Table(name = "ro_person")
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "person_id")
     private Long personId;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "patronymic")
     private String patronymic;
+    @Column(name = "date_birth")
     private LocalDate dateOfBirth;
+    @OneToMany(cascade = {CascadeType.REFRESH}, fetch = FetchType.LAZY, mappedBy = "person")
     private List<Passport> passports;
 
     public Long getPersonId() {
