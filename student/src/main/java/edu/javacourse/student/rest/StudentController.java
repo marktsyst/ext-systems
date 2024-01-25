004b51fa2627e140da1e793a6f3bc98cacd0b15d
marktsyst <mark@tsyst.ru>
2024-01-25 18:18:40 +1000

    rest file 148

--- student/src/main/java/edu/javacourse/student/rest/StudentController.java
+++ student/src/main/java/edu/javacourse/student/rest/StudentController.java
@@ -6,7 +6,10 @@ import edu.javacourse.student.view.StudentResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -27,9 +30,23 @@ public class StudentController {
         return "Rest service is working";
     }
 
-    @GetMapping(path = "/params/{checkId}")
+    @GetMapping(path = "/params/{checkId}/{checkYear}")
     public String checkParams(@PathVariable("checkId") Long checkId,
+                              @PathVariable("checkYear") Long checkYear,
                               @RequestParam("comment") String comment){
-        return checkId + ":" + comment;
+        return checkId + ":" + checkYear + ":" + comment;
+    }
+
+    @PostMapping(path = "/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String uploadPhoto(@RequestParam("comment") String comment,
+                              @RequestParam("photoFile") MultipartFile photoFile) {
+        try (InputStream is = photoFile.getInputStream()) {
+            return "Comment:" + comment +
+                    ", Name:" + photoFile.getName() +
+                    ", File Name:" + photoFile.getOriginalFilename() +
+                    ", Size:" + is.available();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 }
